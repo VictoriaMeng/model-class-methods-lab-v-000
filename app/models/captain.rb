b5fa6bb32ed410d15629d0ddada959bb372435f3
Victoria Meng <nights1ststar@protonmail.com>
2018-07-24 20:09:20 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,19 +1,23 @@
 class Captain < ActiveRecord::Base
   has_many :boats
 
-  def Captain.catamaran_operators
+  def self.catamaran_operators
     Captain.includes(boats: :classifications).where(classifications: {name: "Catamaran"}).uniq
   end
 
-  def Captain.motorboat_operators
+  def self.sailors
+    Captain.includes(boats: :classifications).where(classifications: {name: "Sailboat"} ).uniq
+  end
+
+  def self.motorboat_operators
     Captain.includes(boats: :classifications).where(classifications: {name: "Motorboat"} ).uniq
   end
 
-  def Captain.talented_seafarers
+  def self.talented_seafarers
     Captain.where('id in (?)', Captain.sailors.pluck(:id) & Captain.motorboat_operators.pluck(:id))
   end
 
-  def Captain.non_sailors
+  def self.non_sailors
     Captain.where('id not in (?)', Captain.sailors.pluck(:id))
   end
 end
